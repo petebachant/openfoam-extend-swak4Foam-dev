af84ca6c965918b35f0812af64608204d45b397c
Pete Bachant <petebachant@gmail.com>
2016-02-27 23:52:27 -0500

    Fix shebang line in install script

--- ci/installOF.sh
+++ ci/installOF.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env
+#!/usr/bin/env sh
 # This script installs OpenFOAM on the system
 # The first arg should be the OpenFOAM version
 
-if [ $1 = "dev" ]
+if [ "$1" = "dev" ]
 then
     cd ~/OpenFOAM
     git clone https://github.com/OpenFOAM/OpenFOAM-dev.git
@@ -15,7 +15,7 @@ then
     tar -xf ThirdParty-dev.linux64GccDPInt32Opt_2016-02-27.tbz
     cd $WM_PROJECT_DIR
     wmakeLnIncludeAll
-elif [ $1 = "3.0" ]
+elif [ "$1" = "3.0" ]
 then
     sudo add-apt-repository http://www.openfoam.org/download/ubuntu
     sudo apt-get update -qq
